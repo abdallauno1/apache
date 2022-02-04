df5f38cdb38e98e0841275b45aa5fd1a4e68a95e
Abdalla <abdullamady@hotmail.com>
2022-02-04 11:30:38 +0100

    install apache

---
+++ recipes/default.rb
@@ -0,0 +1,24 @@
+package 'Install Apache' do
+		  case node[:platform]
+		  when 'redhat', 'centos'
+		    package_name 'httpd'
+		  when 'ubuntu', 'debian'
+		    package_name 'apache2'
+		  end
+		end
+
+		# Do something
+
+		service 'Service' do
+		  case node[:platform]
+		  when 'redhat' , 'centso'
+		   service_name 'httpd'
+		        action [:enable, :start]
+		  when 'ubuntu' , 'debian'
+		   service_name 'apache2'
+		        action [:enable, :start]
+		  end
+		end
+		file '/var/www/html/index.html' do
+		content 'Ciao Younes - Lara , abbiamo completato the chef installaion :)'
+		end
