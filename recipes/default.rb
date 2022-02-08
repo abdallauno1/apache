f2e3dd3ee253a36fb6df1fe22bcddd8546b6bb89
Abdalla <abdullamady@hotmail.com>
2022-02-08 15:04:12 +0100

    Update default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -27,7 +27,8 @@ package 'Install Apache' do
 				<head>
 				</head>
 				<style>
-				p { color: #333333; font-family: "Helvetica Neue",Arial,sans-serif; font-size: 16px; font-weight: 300; line-height: 1.5625; margin-bottom: 15px; }
+				p { color: #333333; border: 2px solid powderblue; margin: 50px; font-family: "Helvetica Neue",Arial,sans-serif; 
+					font-size: 16px; font-weight: 300; line-height: 1.5625; margin-bottom: 15px; }
 				</style>
 					<body>
 					<p>Hello Steve, There is Mady fro DevOps</p><br><br>
